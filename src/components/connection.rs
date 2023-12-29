f23f2ba15bc3fc54b3ec645522fbeac51bd67698
Piotr Szokalski <piotr.szokalski0022@gmail.com>
2023-12-29 17:39:28 +0100

    test - connetion serialzont

--- src/components/connection.rs
+++ src/components/connection.rs
@@ -1,7 +1,7 @@
 use serde::{de::SeqAccess, de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
 use std::sync::{Arc, Mutex};
 /// Shared data used to connect vms, analogs to a wire connecting them
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Connection {
     data: Arc<Mutex<i32>>,
 }
@@ -17,43 +17,43 @@ impl Connection {
     }
 }
 
-impl Serialize for Connection {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        self.data.lock().unwrap().serialize(serializer)
-    }
-}
+// impl Serialize for Connection {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer,
+//     {
+//         self.data.lock().unwrap().serialize(serializer)
+//     }
+// }
 
-impl<'de> Deserialize<'de> for Connection {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct ConnectionVisitor;
+// impl<'de> Deserialize<'de> for Connection {
+//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+//     where
+//         D: Deserializer<'de>,
+//     {
+//         struct ConnectionVisitor;
 
-        impl<'de> Visitor<'de> for ConnectionVisitor {
-            type Value = Connection;
+//         impl<'de> Visitor<'de> for ConnectionVisitor {
+//             type Value = Connection;
 
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("an i32 inside an Arc<Mutex<>>")
-            }
+//             fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+//                 formatter.write_str("an i32 inside an Arc<Mutex<>>")
+//             }
 
-            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-            where
-                A: SeqAccess<'de>,
-            {
-                let inner_value = seq
-                    .next_element()?
-                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
+//             fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+//             where
+//                 A: SeqAccess<'de>,
+//             {
+//                 let inner_value = seq
+//                     .next_element()?
+//                     .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
 
-                Ok(Connection {
-                    data: Arc::new(Mutex::new(inner_value)),
-                })
-            }
-        }
+//                 Ok(Connection {
+//                     data: Arc::new(Mutex::new(inner_value)),
+//                 })
+//             }
+//         }
 
-        deserializer.deserialize_seq(ConnectionVisitor)
-    }
-}
+//         deserializer.deserialize_seq(ConnectionVisitor)
+//     }
+// }
