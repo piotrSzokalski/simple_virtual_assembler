9d7c41542c3267f8bbdee37d60a091dbcc2a2326
Piotr Szokalski <piotr.szokalski0022@gmail.com>
2024-01-01 12:41:33 +0100

    utility function for getting port ids form connection

--- src/components/connection.rs
+++ src/components/connection.rs
@@ -10,9 +10,11 @@ pub struct Connection {
 }
 
 impl Connection {
-
     pub fn new() -> Connection {
-        Connection { data: Arc::new(Mutex::new(0)), ports: Vec::new() }
+        Connection {
+            data: Arc::new(Mutex::new(0)),
+            ports: Vec::new(),
+        }
     }
 
     pub fn get(&mut self) -> Arc<Mutex<i32>> {
@@ -20,16 +22,34 @@ impl Connection {
     }
     /// Helper to manage connections
     /// Mainly to rebuild connections in process of deserialization
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// * id String - arbitrary name used to identify connected port
-    pub fn add_port_id(&mut self,id: String) {
+    pub fn add_port_id(&mut self, id: String) {
         self.ports.push(id);
     }
 
     pub fn remove_port_id(&mut self, id: String) {
-        self.ports.retain(|_id| _id != &id); 
+        self.ports.retain(|_id| _id != &id);
+    }
+
+    pub fn get_connected_ports_ids(&self) -> &Vec<String> {
+        &self.ports
+    }
+
+    pub fn get_connected_vms_and_ports(&mut self, delimiter: char) -> Vec<(i32, usize)> {
+        let x: Vec<(i32, usize)> = self
+            .ports
+            .iter()
+            .map(|id| {
+                let split = id.split(delimiter).collect::<Vec<&str>>();
+                let vm_id = split[0].parse::<i32>().unwrap();
+                let port_index = split[1].parse::<usize>().unwrap();
+                (vm_id, port_index)
+            })
+            .collect();
+        x
     }
 }
 
@@ -77,5 +97,17 @@ impl Connection {
 mod test {
     use super::*;
 
-
+    #[test]
+    fn test_getting_connected_vms_and_ports_list() {
+        let port_ids = Vec::from([
+            "0P0", "0P1", "0P2", "0P3", "1P0", "1P1", "1P2", "1P3", "1P3", //
+            "10P3", "9993P3",
+        ]);
+        let mut connection = Connection::new();
+        for id in port_ids.iter() {
+            connection.add_port_id(id.to_string());
+        }
+        let result = connection.get_connected_vms_and_ports('P');
+        print!("{:?}", result);
+    }
 }
