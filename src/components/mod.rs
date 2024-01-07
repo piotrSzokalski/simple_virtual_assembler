e67d369048dbd13775e5ab933c1792846ed37d1b
Piotr Szokalski <piotr.szokalski0022@gmail.com>
2024-01-07 16:35:26 +0100

    ram

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,2 +1,3 @@
 pub mod port;
 pub mod connection;
+pub mod ram;
---
+++ src/components/ram.rs
@@ -0,0 +1,39 @@
+use serde::{Serialize, Deserialize};
+
+use super::port::Port;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Ram {
+    id: Option<usize>,
+    index: usize,
+    data: Vec<i32>,
+    index_port: Port,
+    data_port: Port,
+}
+
+impl Ram {
+    pub fn new() -> Self {
+        Self {
+            id: None,
+            index: 0,
+            data: vec![0; 32],
+            index_port: Port::new(0),
+            data_port: Port::new(0),
+        }
+    }
+    pub fn new_with_id(id: usize) -> Self {
+        Self {
+            id: Some(id),
+            index: 0,
+            data: vec![0; 32],
+            index_port: Port::new(0),
+            data_port: Port::new(0),
+        }
+    }
+
+    pub fn refresh(&mut self) {
+        self.index = self.index_port.get().try_into().unwrap_or(0);
+        self.data[self.index] = self.data_port.get();
+    }
+}
+
